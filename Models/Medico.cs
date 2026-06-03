1167e70b87a4b1755e76e326e2e2efee87b2e962
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-06-02 23:13:50 -0600

    Agregar archivos de proyecto.

---
+++ Models/Medico.cs
@@ -0,0 +1,12 @@
+﻿namespace CitasApp.Models
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+
+        public String Especialidad { get; set; }
+        public String NumeroLicencia { get; set; }
+    }
+}
