d38d8109c30d4fea28d02a70aa0acbbd3617cfb7
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-06-13 03:37:32 -0600

    FEAT: Agregar datos tipo csv, y hacer pruebas de demostración de swap de adapter

--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
+using CitasApp.Infrastructure.Repositories;
 using CitasApp.Interfaces;
 using CitasApp.Repositories;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Carpeta de datos
+var dataFolder = Path.Combine(builder.Environment.ContentRootPath, "data");
+
+Directory.CreateDirectory(dataFolder);
+
+// Archivos CSV
+var csvPacientes = Path.Combine(dataFolder, "pacientes.csv");
+var csvMedicos = Path.Combine(dataFolder, "medicos.csv");
+var csvCitas = Path.Combine(dataFolder, "citas.csv");
+
+// Repositorios CSV
+/*builder.Services.AddScoped<IPacienteRepository>(
+    _ => new CsvPacienteRepository(csvPacientes));
+
+builder.Services.AddScoped<IMedicoRepository>(
+    _ => new CsvMedicoRepository(csvMedicos));
+
+builder.Services.AddScoped<ICitaRepository>(
+    _ => new CsvCitaRepository(csvCitas)); */
+
+
+// a Json
 builder.Services.AddScoped<IPacienteRepository, JsonPacienteRepository>();
 builder.Services.AddScoped<IMedicoRepository, JsonMedicoRepository>();
 builder.Services.AddScoped<ICitaRepository, JsonCitaRepository>();
@@ -16,7 +40,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -32,6 +55,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-
 app.Run();
-
