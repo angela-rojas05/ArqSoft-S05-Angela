0da9f34406596269166d646878c4668158aa6dc0
angela-rojas05 <angela.rojas@tecdesoftware.edu.mx>
2026-06-16 20:10:37 -0600

    REFACTOR: integrar capas hexagonales dentro del repositorio

--- Program.cs
+++ Program.cs
@@ -1,58 +1,22 @@
+using CitasApp.Application.Services;
+using CitasApp.Domain.Interfaces;
 using CitasApp.Infrastructure.Repositories;
 using CitasApp.Interfaces;
 using CitasApp.Repositories;
-
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-// Carpeta de datos
-var dataFolder = Path.Combine(builder.Environment.ContentRootPath, "data");
-
-Directory.CreateDirectory(dataFolder);
-
-// Archivos CSV
-var csvPacientes = Path.Combine(dataFolder, "pacientes.csv");
-var csvMedicos = Path.Combine(dataFolder, "medicos.csv");
-var csvCitas = Path.Combine(dataFolder, "citas.csv");
-
-// Repositorios CSV
-/*builder.Services.AddScoped<IPacienteRepository>(
-    _ => new CsvPacienteRepository(csvPacientes));
-
-builder.Services.AddScoped<IMedicoRepository>(
-    _ => new CsvMedicoRepository(csvMedicos));
-
-builder.Services.AddScoped<ICitaRepository>(
-    _ => new CsvCitaRepository(csvCitas)); */
-
-
-// a Json
+builder.Services.AddControllers();
+// Repositorios
 builder.Services.AddScoped<IPacienteRepository, JsonPacienteRepository>();
 builder.Services.AddScoped<IMedicoRepository, JsonMedicoRepository>();
-builder.Services.AddScoped<ICitaRepository, JsonCitaRepository>();
-
+builder.Services.AddScoped < ICitaRepository,     
+// Servicios de aplicación
+builder.Services.AddScoped<PacienteService>();
+builder.Services.AddScoped<MedicoService>();
+builder.Services.AddScoped<CitaService>();
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
-app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapControllers();
+JsonCitaRepository > ();
+31
 app.Run();
